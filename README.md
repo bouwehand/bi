b4256605831811ee2c81a8664979c92a6e0467ec
Bas Ouwehand <bas@tracefy.com>
2020-08-06 21:43:18 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+#Money
+***business bussiness***
+
+##Pyhton basics
+- to start the virtual environment:
+
+`python3 -m venv env`
+
+pip install "Django"
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Money Business Business',
+    version='0.1dev',
+    author='B.J.Ouwehand',
+    packages=['Money'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
